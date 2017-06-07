ee711871cd7afb2155a7c558d057adb3bc09578d
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-06-07 20:48:15 +0100

    Extension function, user id to be retrieved

--- unpolarise.js
+++ unpolarise.js
@@ -1,43 +1,11 @@
-// STEPS
+// ----- STEPS -----
 // 1. Open Facebook home and get user id
 // 2. Get external links from Facebook home feed
-// 3. Post request to App
+// 3. Send post request to App
 
-/* TODO Delete */ console.log("Welcome!");
+// ----- FUNCTIONS TO BE CALLED -----
 
-// 1. Open Facebook home and get user id
-/* TODO Delete */ console.log("Searching Facebook user ID...");
-// Setup: https://developers.facebook.com/docs/javascript/quickstart
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId            : '226017737902942',
-//     autoLogAppEvents : true,
-//     xfbml            : true,
-//     version          : 'v2.9'
-//   });
-//   FB.AppEvents.logPageView();
-// };
-// (function(d, s, id){
-//    var js, fjs = d.getElementsByTagName(s)[0];
-//    if (d.getElementById(id)) {return;}
-//    js = d.createElement(s); js.id = id;
-//    js.src = "//connect.facebook.net/en_US/sdk.js";
-//    fjs.parentNode.insertBefore(js, fjs);
-//  }(document, 'script', 'facebook-jssdk'));
-// // Get user id function
-// // Source: https://developers.facebook.com/docs/facebook-login/web#checklogin
-// FB.getLoginStatus(function(response) {
-//   if (response.status === 'connected') {
-//     console.log('Logged in.');
-//     var uid = response.authResponse.userID;
-//   }
-//   else {
-//     console.log("You need to be logged in and signed up to unpolarise");
-//     FB.login();
-//   }
-// });
-
-// 2. Get external links from Facebook home feed
+// Function that gets external links from Facebook home feed
 function getFeed() {
   var elements = document.getElementsByTagName("a");
   var links = [];
@@ -49,22 +17,14 @@ function getFeed() {
   return links;
 };
 
-/* TODO Delete */ console.log("Gathering feed data...");
-var links = getFeed(); // links is an array
-
-// 3. Post request to App
-var feed = {"user_id": "To be updated", "links": links};
-var data = JSON.stringify(feed);
-
-/* TODO Delete */ console.log("Submitting for analysis...");
-
+// Function that sends post request to app
 function sendFeed() {
   $.ajax({
     type: "POST",
-    url: '', // To be updated, temporarily use: https://requestb.in/
-    data: data,
+    url: 'https://unpolarise.herokuapp.com/links', // DEVELOPMENT: use 'https://requestb.in/'
+    data: feed,
     success: function() {
-      console.log(data);
+      console.log(feed);
     },
     error: function(error) {
       console.log(error.statusText);
@@ -72,10 +32,80 @@ function sendFeed() {
   });
 };
 
+// Function that gets
+$(document).ready(function() {
+
+  $.ajaxSetup({ cache: true });
+  $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+    FB.init({
+      appId: '226017737902942',
+      version: 'v2.9' // or v2.1, v2.2, v2.3, ...
+    });
+    FB._domain = {
+  api : 'https://api.facebook.com/',
+  cdn : 'https://s-static.ak.fbcdn.net/',
+  www : 'https://www.facebook.com/'
+};
+    FB.getLoginStatus(function() {
+      console.log('sfdnsfdsnfjkdsf')
+    });
+  });
+
+});
+
+// Facebook documentation sources:
+// Setup: https://developers.facebook.com/docs/javascript/quickstart
+// Login: https://developers.facebook.com/docs/facebook-login/web#checklogin
+// Facebook app id: '226017737902942'
+
+// Function that initializes Facebook app
+FB.init({
+  appId            : '226017737902942',
+  autoLogAppEvents : true,
+  status           : true,
+  xfbml            : true,
+  version          : 'v2.9'
+});
+(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+// Function that retrieves Facebook user id
+FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    // the user is logged in and has authenticated the app
+    var uid = response.authResponse.userID;
+    console.log('You are logged in.');
+  } else {
+    console.log('You need to login to Facebook.');
+    FB.login();
+  }
+ });
+
+// ----- SCRIPT RUNNING BELOW -----
+
+/* TODO Delete */ console.log("Welcome!");
+
+/* TODO Delete */ console.log("Searching Facebook user ID...");
+
+// TODO Call function that searches Facebook ID
+
+/* TODO Delete */ console.log("Gathering feed data...");
+
+var urls = getFeed(); // urls is an array
+
+var feed = {"uid": "10155031040388001", "urls": urls};
+// // TODO Replace uid with actual uid
+
+/* TODO Delete */ console.log("Submitting for analysis...");
+
 sendFeed();
 
 /* TODO Delete */ console.log("Done!!!");
 
-
-// TEST prepending in home feed $('#stream_pagelet').prepend('link to app');
+// TODO prepend in home feed $('#stream_pagelet').prepend('link to app');
 
