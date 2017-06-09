140e7c3f64a53a9b4acdf401c82d43952480db41
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-06-09 17:00:13 +0100

    Still testing

--- unpolarise.js
+++ unpolarise.js
@@ -6,7 +6,7 @@
 // ----- GLOBAL VARIABLES AND FUNCTIONS TO BE CALLED -----
 
 // Global variables
-var uid = 'dummy' // Facebook ID
+var email = 'dummy' // Facebook ID
 var unpolariseUrl = /unpolarise\.herokuapp\.com/;
 var facebookUrl = /facebook\.com/;
 var currentUrl = location.href;
@@ -38,75 +38,38 @@ function sendFeed() {
   });
 };
 
-// Facebook documentation sources:
-// Setup: https://developers.facebook.com/docs/javascript/quickstart
-// Login: https://developers.facebook.com/docs/facebook-login/web#checklogin
-function getFacebookId() {
-  var apiLoaded = false;
-
-  window.fbAsyncInit = function() {
-    (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-
-    FB.init({
-      appId      : '226017737902942',
-      status     : true,
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.9'
-    });
-
-    FB.getLoginStatus(function(response) {
-      if (response.status === 'connected') {
-        // the user is logged in and has authenticated the app
-        console.log('You are logged in.');
-        uid = response.authResponse.userID;
-      } else {
-        console.log('You need to login to Facebook.');
-        FB.login();
-      }
-     });
-  };
-};
-
 // ----- SCRIPT RUNNING -----
 
-// Script when active tab is unpolarise (runs until uid received)
-// while (uid === 'dummy') {
-  if (unpolariseUrl.exec(currentUrl)) {
-    /* TODO Delete */ console.log("Requesting your Facebook user ID...");
-    // setTimout(
-      getFacebookId()
-      // , 3000);
-  };
-// };
+// ----- POP UP USER JOURNEYS -----
+var x = document.getElementById('submit');
+x.onclick = function() {
+  email = document.getElementById('user-email').value;
+  console.log(email);
+}
+
+// Link to sign up on unpolarize if not registered
+if (email === 'dummy') { $("#btn").html('<a href="https://unpolarise.herokuapp.com/users/sign_up">Sign up to unpolarise</a>') }
+// Link to get analysis if logged in on Facebook
+else { $("#form").empty();
+   if (facebookUrl.exec(currentUrl)) { $("#btn").html('<a href="https://unpolarise.herokuapp.com/analytics">Get Facebook Feed Analytics</a>') }
+  // Link to open Facebook when registered on website
+  else { $("#btn").html('<a href="https://www.facebook.com/">Screen Facebook Feed</a>') }
+};
 
 // Script when active tab is Facebook (runs every hour)
-if (uid != 'dummy' && facebookUrl.exec(currentUrl)) {
+if (email != 'dummy' && facebookUrl.exec(currentUrl)) {
   // setInterval({
     /* TODO Delete */ console.log("Welcome!");
-    /* TODO Delete */ console.log("Your Facebook  ID: " + uid);
+    /* TODO Delete */ console.log("Your Facebook ID: " + email);
     /* TODO Delete */ console.log("Gathering feed data...");
     var urls = getFeed(); // urls is an array
-    var feed = {"uid": uid, "urls": urls};
+    var feed = {"email": email, "urls": urls};
     /* TODO Delete */ console.log("Submitting for analysis...");
     sendFeed();
     /* TODO Delete */ console.log("Done!!!");
   // }, 3600000 );
 };
 
-// ----- POP UP USER JOURNEYS -----
-// Link to sign up on unpolarize if not registered
-if (uid === 'dummy') { $("#btn").html('<a href="https://unpolarise.herokuapp.com/users/sign_up">Sign up to unpolarise</a>') }
-// Link to get analysis if logged in on Facebook
-else if (facebookUrl.exec(currentUrl)) { $("#btn").html('<a href="https://unpolarise.herokuapp.com/analytics">Get Facebook Feed Analytics</a>') }
-// Link to open Facebook when registered on website
-else { $("#btn").html('<a href="https://www.facebook.com/">Screen Facebook Feed</a>') };
 
 // ----- CHERRY ON THE CAKE -----
 // TODO prepend in home feed $('#stream_pagelet').prepend('link to app');
