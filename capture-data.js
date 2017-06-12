fa5208c10dbb3eae7ba76874e408f485184ffc4e
Edward Stevinson <edward.stevinson@hotmail.com>
2017-06-12 15:16:40 +0100

    Working scraping for likes

--- capture-data.js
+++ capture-data.js
@@ -1,59 +1,45 @@
 /**
  * HTTP GET request function for the facebook page
  */
-function get(url) {
+function getEdit(url, done) { //, done
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
-  // xhr.onreadystatechange = function (e) {
-  //   if (xhr.readyState == 4) {
-  //     done(xhr.responseText);
-  //   }
-  // }
-  // If specified, responseType must be empty string or "document"
-  xhr.responseType = 'document';
-  // overrideMimeType() can be used to force the response to be parsed as XML
-  xhr.overrideMimeType('text/xml');
-  xhr.onload = function () {
-    if (xhr.readyState === xhr.DONE) {
-      if (xhr.status === 200) {
-        console.log(xhr.response);
-      }
+  // xhr.responseType = 'text'; // document
+  // xhr.overrideMimeType('text/xml');
+  xhr.onreadystatechange = function (e) {
+    if (xhr.readyState == 4) {
+      var count = done(xhr.responseText);
     }
-  };
-
+  }
   xhr.send();
   return xhr;
+}
+
+/**
+ * Get the the number of page likes from a Facebook page
+ */
+function getNumberOfPageLikes(url, done) {
+  getEdit(url, function(text) {
+    var count = 0;
+    var profileUrls = $(text).find('h4:contains("Friends who like this ")').siblings().find('a').map(function () {
+      count += 1;
+    }).get();
+    // console.log("Count: " + count);
+    done(count);
+    return count;
+  });
 }
 
 /**
  * Main code that is run when the button is pressed
  */
 document.getElementById("btn-collect").addEventListener('click', () => {
-  console.log("A");
+  console.log("Scraping specified page...");
   var url = "https://mbasic.facebook.com/thedailyshow/socialcontext";
-  // getPageLikesDirect();
-  var test = get(url)
-  // console.log(test.responseXML());
+  var array = [];
+  // Make GET request and count people on page (i.e. the number of friends who have liked)
+  var test2 = getNumberOfPageLikes(url, function(count) {
+    array.push(count);
+  });
+  console.log(array);
 });
-
-/**
- * Get the page likes from a Facebook page
- */
-// function getPageLikesDirect() {
-//   // The url of the page whose like's we want
-//   var url = "https://mbasic.facebook.com/thedailyshow/socialcontext";
-//   console.log("B");
-//   // GET request - jQuery.get( url [, data ] [, success ] [, dataType ] )
-//   // data: A plain object or string that is sent to the server with the request.
-//   get (url, function(text) {
-//     console.log("C");
-//     var profileUrls = $(text).find('h4:contains("Friends who like this ")').siblings().find('a').map(function () {
-//       return {
-//         href: $(this).attr('href'), // We want to count - not keep track of names so this can be changed
-//         name: $(this).text(),
-//       };
-//     }).get();
-//     onFetch();
-//     done(profileUrls);
-//   });
-// }
