54f4596ec2b061d7e8ec060bf11ce0c7cf5271e2
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-06-12 16:12:15 +0100

    v0.0.0.1 published

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 // ----- POP UP USER JOURNEYS -----
 // Link to sign up on unpolarize if not registered
-$("#btn").html('<a href="https://unpolarise.herokuapp.com/users/sign_up">Sign up to unpolarise</a>') };
+$("#btn").html('<a href="https://unpolarise.herokuapp.com/users/sign_up">Sign up to unpolarise</a>');
 // Link to get analysis if logged in on Facebook
 var currentUrl = location.href;
 var facebookUrl = /facebook\.com\/?$/;
